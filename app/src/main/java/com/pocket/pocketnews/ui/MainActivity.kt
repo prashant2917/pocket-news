491a9fe32fd9999ee0231db421739da44f33062f
Prashant Jondhale <prashantj@bruviti.com>
2020-04-03 12:50:56 +0530

    move mainactivity to ui package

--- app/src/main/java/com/pocket/pocketnews/ui/MainActivity.kt
+++ app/src/main/java/com/pocket/pocketnews/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pocket.pocketnews
+package com.pocket.pocketnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pocket.pocketnews.R
 
 class MainActivity : AppCompatActivity() {
 
