5007d1bc1de0e35303336f5b02692e3d7cece648
Prashant Jondhale <prashantj@bruviti.com>
2020-04-03 12:31:40 +0530

    first commit
    initial commit

---
+++ app/src/main/java/com/pocket/pocketnews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pocket.pocketnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
